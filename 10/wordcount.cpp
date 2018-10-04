93dcd7ff63e99ac322459156964f816152dd05c9
William E. Skeith <wes@cs.ccny.cuny.edu>
2018-10-04 12:48:47 -0400

    More notes.

---
+++ 10/wordcount.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+#include <string>
+using std::string;
+#include <vector>
+using std::vector;
+
+/* TODO: finish writing this example from class, and extend it
+ * to work even if the string has newlines or tabs in it. */
+size_t countwords(const string& s)
+{
+	return 0;
+}
+
+/* TODO: write this modified version which, in addition to returning the count
+ * of the words, also stores the individual words in the vector l. */
+size_t countwords(const string& s, vector<string>& l)
+{
+	return 0;
+}
+
+/* TODO: make sure you have done all the exercises from lecture 09/ */
+
+int main()
+{
+	/* TODO: test code for the above goes here. */
+	return 0;
+}
