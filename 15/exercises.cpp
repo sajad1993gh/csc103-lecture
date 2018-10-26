1cbde69e017008f35cbad596fb3236dd08dfb9be
William E. Skeith <wes@cs.ccny.cuny.edu>
2018-10-25 20:44:14 -0400

    Forgot to give any exercises...

---
+++ 15/exercises.cpp
@@ -0,0 +1,15 @@
+/* TODO: re-write the frequency table example from scratch and make sure you
+ * understand why it works. */
+
+/* TODO: try to solve the frequency table problem *WITHOUT* using maps!  Can
+ * you do it with just vectors?  How about just sets?  */
+/* TODO: if you managed to solve the above, estimate the number of
+ * instructions your program will require to run on an input of n strings.
+ * Then compare this with the solution using maps.  Note that M[s]++ takes
+ * approximately log(n) steps if the map has n different strings. */
+/* TODO: write a program that reads lines of text from stdin, printing out
+ * only the first occurrence of each line.  (Basically this is the `uniq`
+ * program already on your computer, but we won't require the input to be
+ * sorted.)  You could use sets or maps for this, btw. */
+/* TODO: Compare/contrast vector<int> with map<int,int>.  (This does not
+ * involve programming, just thinking.) */
