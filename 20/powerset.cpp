700b72343ff2f97993e20de67cf0001ed94b99b0
William E. Skeith <wes@cs.ccny.cuny.edu>
2018-11-13 13:47:57 -0500

    More notes.
    Sorry --  I lost the pdf notes.

---
+++ 20/powerset.cpp
@@ -0,0 +1,48 @@
+#include <set>
+using std::set;
+#include <iostream>
+using std::cin;
+using std::cout;
+
+set<set<int>> powerset(const set<int>& S)
+{
+	if (S.size() == 0) {
+		// return set<set<int>>(set<int>(),1);
+		set<set<int>> P; /* for the return value */
+		set<int> E; /* will be empty! */
+		P.insert(E); /* now P = {{}} */
+		return P;
+	}
+	/* now we assume powerset works on any input of smaller
+	 * size than S (of fewer elements). */
+	set<int> Sp(S); /* make S' a copy of S */
+	int x = *Sp.begin();
+	/* remove x from S': */
+	Sp.erase(x); /* now S' = S\{x} */
+	set<set<int>> T = powerset(Sp);
+	set<set<int>> P(T); /* copy all of T into P */
+	/* what is P missing?  All subsets with an x.  Add those: */
+	for (set<set<int>>::iterator i = T.begin(); i!=T.end(); i++) {
+		set<int> u(*i);
+		u.insert(x);
+		P.insert(u);
+	}
+	return P;
+}
+
+int main()
+{
+	set<int> S = {0,1,2};
+	set<set<int>> P = powerset(S);
+	/* now print result: */
+	cout << "{\n";
+	for (set<set<int>>::iterator i = P.begin(); i!=P.end(); i++) {
+		cout << "  { ";
+		for (set<int>::iterator j = (*i).begin(); j!=(*i).end(); j++) {
+			cout << *j << " ";
+		}
+		cout << "}\n";
+	}
+	cout << "}\n";
+	return 0;
+}
