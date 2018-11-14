0a1e693286fcd68ec814ffab3ed6978c0c2f147e
William E. Skeith <wes@cs.ccny.cuny.edu>
2018-11-13 20:26:00 -0500

    Add exercises.

---
+++ 20/exercises.cpp
@@ -0,0 +1,11 @@
+/* TODO: without looking back at the solution, write the powerset
+ * function from scratch.  Try a version that works on vectors
+ * instead of sets. */
+/* TODO: trace sequence of function calls on a set of 3 elements. */
+/* TODO: try to write another version of the power set function
+ * that divides the set into two disjoint subsets of equal(ish)
+ * size, computes the powersets of those recursively, and then
+ * puts them back together.  It is essentially the same idea as
+ * the above.  Bonus question: prove your algorithm works, and
+ * that it requires approximately the same number of steps as
+ * the original. */
