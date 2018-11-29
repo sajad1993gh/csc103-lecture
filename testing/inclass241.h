9deafd1dc2ec2b9c947771304f5fe04bfee2042c
Sajad Gholamzadehrizi <sajad1993gh@gmail.com>
2018-11-29 18:03:16 -0500

    more

---
+++ testing/inclass241.h
@@ -0,0 +1,10 @@
+//class declaration and member function prototypes
+
+class rectangle{
+  public: //what you need to use the class
+  double w;
+  double h;
+  double area();
+  double perimeter();
+};
+
