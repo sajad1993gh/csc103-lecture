ae3346f4416691ac4b360e8a276e0431ea937b57
Sajad Gholamzadehrizi <sajad1993gh@gmail.com>
2018-12-02 02:17:22 -0500

    practice

---
+++ 24/circle.h
@@ -0,0 +1,10 @@
+//class dec and member func:
+
+#pragma once
+
+class circle {
+  public:
+    double r;
+    double area();
+    double perimeter();
+};
---
+++ README.md
@@ -0,0 +1,2 @@
+# csc103-new
+Due to some technical problems, this is the new repository for saving my work starting from lecture17
