807bc1209a69825a4bb3d9dec294ef5dd78b985c
Sajad Gholamzadehrizi <sajad1993gh@gmail.com>
2018-12-06 11:34:22 -0500

    revision

--- testing/inclass261.cpp
+++ testing/inclass261.cpp
@@ -1,8 +1,11 @@
 //continuation of vector class:::
+#incldue "vector.h"
 
 
+#define NDEBUG 1
 int& vector::operator[](size_t i)
 {
+  assert(i<size); // NOTE:: if you have #defined NDEBUG, the assert disappears before eaching the compiler! no performance cost
   return data[i]; // X[i] == *(X+i)
 }
 
@@ -11,4 +14,9 @@ vector::vector(size_t initCapacity)
   size = 0;
   capacity = initCapacity;
   data = new int[capacity]; // allocate storage for vector's elements
+}
+
+vector::~vector()
+{
+  delete[] data;
 }
