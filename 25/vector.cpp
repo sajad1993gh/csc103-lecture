8222ead44bb22235128faa1e437beb5bfa399ea4
Sajad Gholamzadehrizi <sajad1993gh@gmail.com>
2018-12-14 18:40:15 -0500

    final practice

--- 25/vector.cpp
+++ 25/vector.cpp
@@ -1,5 +1,7 @@
 #include "vector.h"
-
+#include<cassert>
+#include<algorithm>
+using std::swap;
 /* TODO: see if you can fill out some of the other member functions,
  * as well as fixing push_back below so that it works if the capacity
  * is already reached. */
@@ -12,13 +14,72 @@
 
 void vector::pop_back()
 {
-	/* assuming vector not empty */
-	--size;
+	assert(size > 0);
+	/* as long as vector isn't empty, just do this: */
+   size--;
 }
 
 void vector::push_back(int x)
 {
-	/* assuming capacity is large enough: */
+	/* assuming capacity is large enough:
+	just do ::  data[size++] = x;
+ */
+	if (size == capacity) {
+		/* need to allocate more space. */
+		capacity *= 2;
+		int* olddata = data;
+		data = new int[capacity];
+		for (size_t i = 0; i < size; i++) {
+			data[i] = olddata[i];
+		}
+		delete [] olddata;
+	}
 	data[size++] = x;
 }
+
+int& vector::operator[](size_t i) {
+	assert(i < size);
+	return data[i];
+}
+
+vector::vector(size_t initCapacity)
+{
+	size = 0;
+	capacity = initCapacity;
+	data = new int[capacity];
+}
+
+vector::vector(const vector& V)
+{
+	size = V.size;
+	capacity = V.capacity;
+	// data = V.data;
+	/* NO!  Above will end in disaster. TODO: make sure you understand why. */
+	/* need to make a separate copy of V.data. */
+	data = new int[capacity];
+	for (size_t i = 0; i < size; i++) {
+		data[i] = V.data[i];
+	}
+}
+
+
+vector::~vector()
+{
+	delete [] data;
+}
+
+vector& vector::operator=(vector V)
+{
+	/* swap data pointers of *this and V: */
+	swap(data,V.data);
+	/* copy size and capacity from V */
+	size = V.size;
+	capacity = V.capacity;
+	return (*this);
+}
+
+
+
+
+
 
